75e0e167932093d45abe33b9b4230357fdef3bda
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-30 22:01:00 +0700

    Displayed clicked numbers

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const display = document.querySelector(".display");
+const numButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector("#equals");
+const pointButton = document.querySelector("#point");
+const delButton = document.querySelector("#delete");
+const clearButton = document.querySelector("#clear");
+
+let num1 = undefined;
+let num2 = undefined;
+let operator = "";
+
+function initializeEventListeners() {
+    numButtons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            let num = e.target.textContent;
+            updateNum(num);
+            updateDisplay();
+        })
+    });
+}
+
 function add(a, b) {
     a = Number.parseInt(a);
     b = Number.parseInt(b);
@@ -41,3 +63,13 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+function updateNum(val) {
+    num1 = val;
+}
+
+function updateDisplay() {
+    display.textContent = num1;
+}
+
+initializeEventListeners();
